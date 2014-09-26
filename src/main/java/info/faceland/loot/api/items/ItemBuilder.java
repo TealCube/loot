46f477e7e16f6fe03668846625a8faf47a6d2ce6
Richard Harrah <topplethenunnery@gmail.com>
2014-09-26 13:00:16 -0400

    adding ItemBuilder interface

---
+++ src/main/java/info/faceland/loot/api/items/ItemBuilder.java
@@ -0,0 +1,19 @@
+package info.faceland.loot.api.items;
+
+import info.faceland.hilt.HiltItemStack;
+import info.faceland.loot.api.tier.Tier;
+import org.bukkit.Material;
+
+public interface ItemBuilder {
+
+    boolean isBuilt();
+
+    HiltItemStack build();
+
+    ItemBuilder withTier(Tier t);
+
+    ItemBuilder withMaterial(Material m);
+
+    ItemBuilder withItemGenerationReason(ItemGenerationReason reason);
+
+}
