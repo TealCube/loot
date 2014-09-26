34d3b4e2624612b1d91103a250d083372a500329
Richard Harrah <topplethenunnery@gmail.com>
2014-09-26 14:23:36 -0400

    adding method to get new ItemBuilder

--- src/main/java/info/faceland/loot/LootPlugin.java
+++ src/main/java/info/faceland/loot/LootPlugin.java
@@ -5,6 +5,7 @@ import info.faceland.facecore.shade.nun.ivory.config.VersionedIvoryConfiguration
 import info.faceland.facecore.shade.nun.ivory.config.VersionedIvoryYamlConfiguration;
 import info.faceland.facecore.shade.nun.ivory.config.settings.IvorySettings;
 import info.faceland.loot.api.groups.ItemGroup;
+import info.faceland.loot.api.items.ItemBuilder;
 import info.faceland.loot.api.managers.ItemGroupManager;
 import info.faceland.loot.api.managers.NameManager;
 import info.faceland.loot.api.managers.TierManager;
@@ -12,6 +13,7 @@ import info.faceland.loot.api.tier.Tier;
 import info.faceland.loot.api.tier.TierBuilder;
 import info.faceland.loot.groups.LootItemGroup;
 import info.faceland.loot.io.SmartTextFile;
+import info.faceland.loot.items.LootItemBuilder;
 import info.faceland.loot.managers.LootItemGroupManager;
 import info.faceland.loot.managers.LootNameManager;
 import info.faceland.loot.managers.LootTierManager;
@@ -226,6 +228,10 @@ public final class LootPlugin extends FacePlugin {
 
     public TierBuilder getNewTierBuilder(String name) {
         return new LootTierBuilder(name);
+    }
+
+    public ItemBuilder getNewItemBuilder() {
+        return new LootItemBuilder(this);
     }
 
     public TierManager getTierManager() {
