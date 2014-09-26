ed6dc819f3cb853e78cc2abc9caac54b101511d8
Richard Harrah <topplethenunnery@gmail.com>
2014-09-26 11:07:15 -0400

    adding StringConverter
    will eventually be pulled into Facecore

---
+++ src/main/java/info/faceland/loot/utils/converters/StringConverter.java
@@ -0,0 +1,60 @@
+package info.faceland.loot.utils.converters;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+
+public final class StringConverter {
+
+    private StringConverter() {
+        // do nothing
+    }
+
+    public static int toInt(String s, int def) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException | NullPointerException e) {
+            return def;
+        }
+    }
+
+    public static int toInt(String s) {
+        return toInt(s, 0);
+    }
+
+    public static double toDouble(String s, double def) {
+        try {
+            return Double.parseDouble(s);
+        } catch (NumberFormatException | NullPointerException e) {
+            return def;
+        }
+    }
+
+    public static double toDouble(String s) {
+        return toDouble(s, 0D);
+    }
+
+    public static Material toMaterial(String s, Material def) {
+        try {
+            return Material.valueOf(s);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
+    public static Material toMaterial(String s) {
+        return toMaterial(s, Material.AIR);
+    }
+
+    public static Enchantment toEnchantment(String s, Enchantment def) {
+        Enchantment e = Enchantment.getByName(s);
+        if (e == null) {
+            return def;
+        }
+        return e;
+    }
+
+    public static Enchantment toEnchantment(String s) {
+        return toEnchantment(s, Enchantment.ARROW_DAMAGE);
+    }
+
+}
