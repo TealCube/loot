560c8567343626a20bdc9134c43e2b96a7f215f6
Richard Harrah <topplethenunnery@gmail.com>
2014-09-27 19:33:05 -0400

    adding SocketGemManager

---
+++ src/main/java/info/faceland/loot/api/managers/SocketGemManager.java
@@ -0,0 +1,17 @@
+package info.faceland.loot.api.managers;
+
+import info.faceland.loot.api.sockets.SocketGem;
+
+import java.util.Set;
+
+public interface SocketGemManager {
+
+    Set<SocketGem> getSocketGems();
+
+    SocketGem getSocketGem(String name);
+
+    void addSocketGem(SocketGem gem);
+
+    void removeSocketGem(String name);
+
+}
