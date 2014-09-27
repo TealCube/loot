e12d898690c9fb36d241b2f0593fdc9ff3747446
Richard Harrah <topplethenunnery@gmail.com>
2014-09-27 00:42:21 -0400

    adding toItemStack(int) method

--- src/main/java/info/faceland/loot/api/items/CustomItem.java
+++ src/main/java/info/faceland/loot/api/items/CustomItem.java
@@ -1,5 +1,6 @@
 package info.faceland.loot.api.items;
 
+import info.faceland.hilt.HiltItemStack;
 import org.bukkit.Material;
 
 import java.util.List;
@@ -13,5 +14,7 @@ public interface CustomItem {
     List<String> getLore();
 
     Material getMaterial();
+
+    HiltItemStack toItemStack(int amount);
 
 }
