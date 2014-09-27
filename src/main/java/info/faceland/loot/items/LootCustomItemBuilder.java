9b0140c397d6f29ae657262f993eca0ec87500e8
Richard Harrah <topplethenunnery@gmail.com>
2014-09-27 00:49:42 -0400

    adding LootCustomItemBuilder

---
+++ src/main/java/info/faceland/loot/items/LootCustomItemBuilder.java
@@ -0,0 +1,47 @@
+package info.faceland.loot.items;
+
+import info.faceland.loot.api.items.CustomItem;
+import info.faceland.loot.api.items.CustomItemBuilder;
+import org.bukkit.Material;
+
+import java.util.List;
+
+public final class LootCustomItemBuilder implements CustomItemBuilder {
+
+    private boolean built = false;
+    private LootCustomItem customItem;
+
+    public LootCustomItemBuilder(String name) {
+        this.customItem = new LootCustomItem(name);
+    }
+
+    @Override
+    public boolean isBuilt() {
+        return built;
+    }
+
+    @Override
+    public CustomItem build() {
+        this.built = true;
+        return customItem;
+    }
+
+    @Override
+    public CustomItemBuilder withDisplayName(String displayName) {
+        customItem.setDisplayName(displayName);
+        return this;
+    }
+
+    @Override
+    public CustomItemBuilder withLore(List<String> lore) {
+        customItem.setLore(lore);
+        return this;
+    }
+
+    @Override
+    public CustomItemBuilder withMaterial(Material material) {
+        customItem.setMaterial(material);
+        return this;
+    }
+
+}
