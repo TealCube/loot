522ddb73e0fc72a367fa28726290734c156498e3
Richard Harrah <topplethenunnery@gmail.com>
2014-09-27 00:45:52 -0400

    adding CustomItemBuilder interface

---
+++ src/main/java/info/faceland/loot/api/items/CustomItemBuilder.java
@@ -0,0 +1,19 @@
+package info.faceland.loot.api.items;
+
+import org.bukkit.Material;
+
+import java.util.List;
+
+public interface CustomItemBuilder {
+
+    boolean isBuilt();
+
+    CustomItem build();
+
+    CustomItemBuilder withDisplayName(String displayName);
+
+    CustomItemBuilder withLore(List<String> lore);
+
+    CustomItemBuilder withMaterial(Material material);
+
+}
