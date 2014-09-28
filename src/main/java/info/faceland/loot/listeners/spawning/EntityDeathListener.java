54d1b50739b7f75572f4b2d17051fdcd9386d3b7
Richard Harrah <topplethenunnery@gmail.com>
2014-09-28 01:13:49 -0400

    adding EntityDeathListener

---
+++ src/main/java/info/faceland/loot/listeners/spawning/EntityDeathListener.java
@@ -0,0 +1,48 @@
+package info.faceland.loot.listeners.spawning;
+
+import info.faceland.loot.LootPlugin;
+import info.faceland.loot.math.LootRandom;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public final class EntityDeathListener implements Listener {
+
+    private final LootPlugin plugin;
+    private final LootRandom random;
+
+    public EntityDeathListener(LootPlugin plugin) {
+        this.plugin = plugin;
+        this.random = new LootRandom(System.currentTimeMillis());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event instanceof PlayerDeathEvent) {
+            return;
+        }
+        if (event.getEntity().getKiller() == null) {
+            return;
+        }
+        if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.normal-item", 0D)) {
+            // drop a normal random item
+        } else if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.socket-gem", 0D)) {
+            // drop a socket gem
+        } else if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.enchant-gem", 0D)) {
+            // drop an enchant gem
+        } else if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.upgrade-scroll", 0D)) {
+            // drop an upgrade scroll
+        } else if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.identity-tome", 0D)) {
+            // drop an identity tome
+        } else if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.custom-item", 0D)) {
+            // drop a custom item
+        } else if (random.nextDouble() < plugin.getSettings().getDouble("config.drop.socket-extender", 0D)) {
+            // drop a socket extender
+        } else {
+            // do nothing
+        }
+    }
+
+}
