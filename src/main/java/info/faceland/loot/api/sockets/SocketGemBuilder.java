a5d246f83cc39985fccfd1144c75273db1c865f3
Richard Harrah <topplethenunnery@gmail.com>
2014-09-27 20:22:39 -0400

    adding SocketGemBuilder

---
+++ src/main/java/info/faceland/loot/api/sockets/SocketGemBuilder.java
@@ -0,0 +1,25 @@
+package info.faceland.loot.api.sockets;
+
+import info.faceland.loot.api.sockets.effects.SocketEffect;
+
+import java.util.List;
+
+public interface SocketGemBuilder {
+
+    boolean isBuilt();
+
+    SocketGem build();
+
+    SocketGemBuilder withDisplayName(String s);
+
+    SocketGemBuilder withWeight(double d);
+
+    SocketGemBuilder withPrefix(String s);
+
+    SocketGemBuilder withSuffix(String s);
+
+    SocketGemBuilder withLore(List<String> l);
+
+    SocketGemBuilder withSocketEffects(List<SocketEffect> effects);
+
+}
