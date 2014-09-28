6f238bee13e5fb2efa33bf9d4af79c0267cbbc16
Richard Harrah <topplethenunnery@gmail.com>
2014-09-28 18:18:32 -0400

    adding equals() and hashCode() to LootSocketGem

--- src/main/java/info/faceland/loot/sockets/LootSocketGem.java
+++ src/main/java/info/faceland/loot/sockets/LootSocketGem.java
@@ -100,4 +100,23 @@ public final class LootSocketGem implements SocketGem {
         this.itemGroup = itemGroup;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LootSocketGem that = (LootSocketGem) o;
+
+        return !(name != null ? !name.equals(that.name) : that.name != null);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
 }
