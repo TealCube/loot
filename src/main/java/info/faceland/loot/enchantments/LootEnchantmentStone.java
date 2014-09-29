0037459286c3f99e8020cc939e03e1b6fce8b47c
Richard Harrah <topplethenunnery@gmail.com>
2014-09-29 18:07:21 -0400

    adding equals() and hashCode() methods

--- src/main/java/info/faceland/loot/enchantments/LootEnchantmentStone.java
+++ src/main/java/info/faceland/loot/enchantments/LootEnchantmentStone.java
@@ -66,5 +66,24 @@ public final class LootEnchantmentStone implements EnchantmentStone {
     void setMaxStats(int maxStats) {
         this.maxStats = maxStats;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LootEnchantmentStone that = (LootEnchantmentStone) o;
+
+        return !(name != null ? !name.equals(that.name) : that.name != null);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
 }
