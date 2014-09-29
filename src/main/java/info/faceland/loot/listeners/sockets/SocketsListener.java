86d7a6f18e1413e1d01d2f26e490c1c260717417
Richard Harrah <topplethenunnery@gmail.com>
2014-09-29 17:56:24 -0400

    applying sockets on hit

--- src/main/java/info/faceland/loot/listeners/sockets/SocketsListener.java
+++ src/main/java/info/faceland/loot/listeners/sockets/SocketsListener.java
@@ -3,16 +3,23 @@ package info.faceland.loot.listeners.sockets;
 import info.faceland.hilt.HiltItemStack;
 import info.faceland.loot.LootPlugin;
 import info.faceland.loot.api.sockets.SocketGem;
+import info.faceland.loot.api.sockets.effects.SocketEffect;
 import info.faceland.loot.utils.StringListUtils;
 import info.faceland.utils.TextUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class SocketsListener implements Listener {
@@ -21,6 +28,110 @@ public final class SocketsListener implements Listener {
 
     public SocketsListener(LootPlugin plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        List<SocketGem> attackerGems = new ArrayList<>();
+        List<SocketGem> defenderGems = new ArrayList<>();
+        Entity attacker = event.getDamager();
+        Entity defender = event.getEntity();
+        if (attacker instanceof Player) {
+            Player attackerP = (Player) attacker;
+            if (attackerP.getItemInHand() != null && attackerP.getItemInHand().getType() != Material.AIR) {
+                HiltItemStack inHand = new HiltItemStack(attackerP.getItemInHand());
+                List<String> lore = inHand.getLore();
+                List<String> strippedLore = StringListUtils.stripColor(lore);
+                for (String key : strippedLore) {
+                    SocketGem gem = plugin.getSocketGemManager().getSocketGem(key);
+                    if (gem == null) {
+                        continue;
+                    }
+                    attackerGems.add(gem);
+                }
+            }
+        }
+        if (defender instanceof Player) {
+            Player defenderP = (Player) defender;
+            for (ItemStack equipment : defenderP.getEquipment().getArmorContents()) {
+                if (equipment == null || equipment.getType() == Material.AIR) {
+                    continue;
+                }
+                HiltItemStack item = new HiltItemStack(equipment);
+                List<String> lore = item.getLore();
+                List<String> strippedLore = StringListUtils.stripColor(lore);
+                for (String key : strippedLore) {
+                    SocketGem gem = plugin.getSocketGemManager().getSocketGem(key);
+                    if (gem == null) {
+                        continue;
+                    }
+                    defenderGems.add(gem);
+                }
+            }
+        }
+
+        for (SocketGem gem : attackerGems) {
+            for (SocketEffect effect : gem.getSocketEffects()) {
+                switch (effect.getTarget()) {
+                    case SELF:
+                        if (attacker instanceof LivingEntity) {
+                            effect.apply((LivingEntity) attacker);
+                        }
+                        break;
+                    case OTHER:
+                        if (defender instanceof LivingEntity) {
+                            effect.apply((LivingEntity) defender);
+                        }
+                        break;
+                    case AREA:
+                        for (Entity e : defender
+                                .getNearbyEntities(effect.getRadius(), effect.getRadius(), effect.getRadius())) {
+                            if (e instanceof LivingEntity) {
+                                effect.apply((LivingEntity) e);
+                            }
+                        }
+                        if (defender instanceof LivingEntity) {
+                            effect.apply((LivingEntity) defender);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        for (SocketGem gem : defenderGems) {
+            for (SocketEffect effect : gem.getSocketEffects()) {
+                switch (effect.getTarget()) {
+                    case SELF:
+                        if (defender instanceof LivingEntity) {
+                            effect.apply((LivingEntity) defender);
+                        }
+                        break;
+                    case OTHER:
+                        if (attacker instanceof LivingEntity) {
+                            effect.apply((LivingEntity) attacker);
+                        }
+                        break;
+                    case AREA:
+                        for (Entity e : attacker
+                                .getNearbyEntities(effect.getRadius(), effect.getRadius(), effect.getRadius())) {
+                            if (e instanceof LivingEntity) {
+                                effect.apply((LivingEntity) e);
+                            }
+                        }
+                        if (attacker instanceof LivingEntity) {
+                            effect.apply((LivingEntity) attacker);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
