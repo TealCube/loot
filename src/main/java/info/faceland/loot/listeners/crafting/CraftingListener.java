9148a2ffdd936756d5b96eca0417fb1308d4943c
Richard Harrah <topplethenunnery@gmail.com>
2014-10-02 20:27:13 -0400

    adding crafting listener

---
+++ src/main/java/info/faceland/loot/listeners/crafting/CraftingListener.java
@@ -0,0 +1,34 @@
+package info.faceland.loot.listeners.crafting;
+
+import info.faceland.hilt.HiltItemStack;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+public final class CraftingListener implements Listener {
+
+    public CraftingListener() {
+        // do nothing
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onCraftItemEvent(CraftItemEvent event) {
+        for (ItemStack is : event.getInventory().getMatrix()) {
+            if (is == null || is.getType() == Material.AIR) {
+                continue;
+            }
+            HiltItemStack his = new HiltItemStack(is);
+            if (his.getName().equals(ChatColor.DARK_AQUA + "Socket Extender") ||
+                    his.getName().startsWith(ChatColor.BLUE + "Enchantment Tome - ") ||
+                    his.getName().startsWith(ChatColor.GOLD + "Socket Gem -")) {
+                event.setCancelled(true);
+                return;
+            }
+        }
+    }
+
+}
