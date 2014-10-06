6b4449a8f5d1acc10e69a8d6d65c3e3896f47a2b
Richard Harrah <topplethenunnery@gmail.com>
2014-10-05 20:22:45 -0400

    adding ProtectionCharm

---
+++ src/main/java/info/faceland/loot/items/prefabs/ProtectionCharm.java
@@ -0,0 +1,19 @@
+package info.faceland.loot.items.prefabs;
+
+import info.faceland.hilt.HiltItemStack;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+
+import java.util.Arrays;
+
+public final class ProtectionCharm extends HiltItemStack {
+
+    public ProtectionCharm() {
+        super(Material.DIAMOND);
+        setName(ChatColor.AQUA + "Charm of Protection");
+        setLore(Arrays.asList(ChatColor.WHITE + "Prevents one failed upgrade. Consumed on use."));
+    }
+
+
+
+}
