eb01b42a2257d02c38413e6adeabf415b1f3abd4
Richard Harrah <topplethenunnery@gmail.com>
2014-10-10 01:13:28 -0400

    loading trigger text

--- src/main/java/info/faceland/loot/LootPlugin.java
+++ src/main/java/info/faceland/loot/LootPlugin.java
@@ -415,6 +415,7 @@ public final class LootPlugin extends FacePlugin {
             builder.withItemGroups(groups);
             builder.withBroadcast(cs.getBoolean("broadcast"));
             builder.withTriggerable(cs.getBoolean("triggerable"));
+            builder.withTriggerText(cs.getString("trigger-text"));
             SocketGem gem = builder.build();
             gems.add(gem);
             loadedSocketGems.add(gem.getName());
