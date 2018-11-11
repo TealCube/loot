f77a445a00427a7ea3cec5e47dea2fa2ea09b299
UltraFaceguy <kattreinkyle@gmail.com>
2018-11-11 15:42:45 -0500

    formatting

--- src/main/java/info/faceland/loot/listeners/EnchantDegradeListener.java
+++ src/main/java/info/faceland/loot/listeners/EnchantDegradeListener.java
@@ -1,24 +1,20 @@
 /**
- * The MIT License
- * Copyright (c) 2015 Teal Cube Games
+ * The MIT License Copyright (c) 2015 Teal Cube Games
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package info.faceland.loot.listeners;
 
@@ -40,78 +36,91 @@ import org.bukkit.inventory.ItemStack;
 
 public final class EnchantDegradeListener implements Listener {
 
-    private final LootPlugin plugin;
-    private LootRandom random;
+  private final LootPlugin plugin;
+  private LootRandom random;
 
-    public EnchantDegradeListener(LootPlugin plugin) {
-        this.plugin = plugin;
-        this.random = new LootRandom();
-    }
+  public EnchantDegradeListener(LootPlugin plugin) {
+    this.plugin = plugin;
+    this.random = new LootRandom();
+  }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onEntityDeath(EntityDeathEvent event) {
-        LivingEntity dyingEntity = event.getEntity();
-        Player p = dyingEntity.getKiller();
-        if (p == null) {
-            return;
-        }
-        if (random.nextDouble() > plugin.getSettings().getDouble("config.enchantment-degrade", 1.0)) {
-            return;
-        }
-        switch (random.nextInt(6)) {
-            case 0:
-                if (p.getEquipment().getItemInMainHand() == null) return;
-                p.getEquipment().setItemInMainHand(degrade(p.getEquipment().getItemInMainHand(), p));
-                return;
-            case 1:
-                if (p.getEquipment().getItemInOffHand() == null) return;
-                p.getEquipment().setItemInOffHand(degrade(p.getEquipment().getItemInOffHand(), p));
-                break;
-            case 2:
-                if (p.getEquipment().getHelmet() == null) return;
-                p.getEquipment().setHelmet(degrade(p.getEquipment().getHelmet(), p));
-                break;
-            case 3:
-                if (p.getEquipment().getChestplate() == null) return;
-                p.getEquipment().setChestplate(degrade(p.getEquipment().getChestplate(), p));
-                break;
-            case 4:
-                if (p.getEquipment().getLeggings() == null) return;
-                p.getEquipment().setLeggings(degrade(p.getEquipment().getLeggings(), p));
-                break;
-            case 5:
-                if (p.getEquipment().getBoots() == null) return;
-                p.getEquipment().setBoots(degrade(p.getEquipment().getBoots(), p));
-                break;
-        }
+  @EventHandler(priority = EventPriority.MONITOR)
+  public void onEntityDeath(EntityDeathEvent event) {
+    LivingEntity dyingEntity = event.getEntity();
+    Player p = dyingEntity.getKiller();
+    if (p == null) {
+      return;
     }
+    if (random.nextDouble() > plugin.getSettings().getDouble("config.enchantment-degrade", 1.0)) {
+      return;
+    }
+    switch (random.nextInt(6)) {
+      case 0:
+        if (p.getEquipment().getItemInMainHand() == null) {
+          return;
+        }
+        p.getEquipment().setItemInMainHand(degrade(p.getEquipment().getItemInMainHand(), p));
+        return;
+      case 1:
+        if (p.getEquipment().getItemInOffHand() == null) {
+          return;
+        }
+        p.getEquipment().setItemInOffHand(degrade(p.getEquipment().getItemInOffHand(), p));
+        break;
+      case 2:
+        if (p.getEquipment().getHelmet() == null) {
+          return;
+        }
+        p.getEquipment().setHelmet(degrade(p.getEquipment().getHelmet(), p));
+        break;
+      case 3:
+        if (p.getEquipment().getChestplate() == null) {
+          return;
+        }
+        p.getEquipment().setChestplate(degrade(p.getEquipment().getChestplate(), p));
+        break;
+      case 4:
+        if (p.getEquipment().getLeggings() == null) {
+          return;
+        }
+        p.getEquipment().setLeggings(degrade(p.getEquipment().getLeggings(), p));
+        break;
+      case 5:
+        if (p.getEquipment().getBoots() == null) {
+          return;
+        }
+        p.getEquipment().setBoots(degrade(p.getEquipment().getBoots(), p));
+        break;
+    }
+  }
 
-    private ItemStack degrade(ItemStack itemStack, Player player) {
-        if (itemStack == null || itemStack.getItemMeta() == null || itemStack.getItemMeta().getLore() == null) {
-            return itemStack;
-        }
-        HiltItemStack item = new HiltItemStack(itemStack);
-        List<String> lore = new ArrayList<>();
-        for (int i = 0; i < item.getLore().size(); i++) {
-            String string = item.getLore().get(i);
-            if (!string.startsWith(ChatColor.BLUE + "[") || !string.contains("" + ChatColor.BLACK)) {
-                lore.add(string);
-                continue;
-            }
-            int index = string.indexOf("" + ChatColor.BLACK);
-            if (index <= 4) {
-                lore.remove(lore.size() - 1);
-                lore.add(ChatColor.BLUE + "(Enchantable)");
-                sendMessage(player, plugin.getSettings().getString("language.enchant.degrade", ""));
-                continue;
-            } else if (index <= 7) {
-                sendMessage(player, plugin.getSettings().getString("language.enchant.bar-low", ""));
-            }
-            string = string.replace("" + ChatColor.BLACK, "");
-            string = new StringBuilder(string).insert(index - 1, ChatColor.BLACK + "").toString();
-            lore.add(string);
-        }
-        item.setLore(lore);
-        return item;
+  private ItemStack degrade(ItemStack itemStack, Player player) {
+    if (itemStack == null || itemStack.getItemMeta() == null
+        || itemStack.getItemMeta().getLore() == null) {
+      return itemStack;
     }
+    HiltItemStack item = new HiltItemStack(itemStack);
+    List<String> lore = new ArrayList<>();
+    for (int i = 0; i < item.getLore().size(); i++) {
+      String string = item.getLore().get(i);
+      if (!string.startsWith(ChatColor.BLUE + "[") || !string.contains("" + ChatColor.BLACK)) {
+        lore.add(string);
+        continue;
+      }
+      int index = string.indexOf("" + ChatColor.BLACK);
+      if (index <= 4) {
+        lore.remove(lore.size() - 1);
+        lore.add(ChatColor.BLUE + "(Enchantable)");
+        sendMessage(player, plugin.getSettings().getString("language.enchant.degrade", ""));
+        continue;
+      } else if (index <= 7) {
+        sendMessage(player, plugin.getSettings().getString("language.enchant.bar-low", ""));
+      }
+      string = string.replace("" + ChatColor.BLACK, "");
+      string = new StringBuilder(string).insert(index - 1, ChatColor.BLACK + "").toString();
+      lore.add(string);
+    }
+    item.setLore(lore);
+    return item;
+  }
 }
